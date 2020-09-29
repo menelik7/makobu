7c50607438fed66f162e18e51a28f124e09a98d7
Menelik Tefera <menelikworku@gmail.com>
2020-09-29 16:56:44 -0400

    Body background changed and web under dev styling fixed

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -55,20 +55,15 @@ class Landing extends React.Component {
 
 	render() {
 		return (
-			<div className="container landing-main-div">
-				<div className="col-lg-6 offset-lg-3 alert alert-secondary grey website-under-dev">
-					<div className="row">
-						<div className="col-2 col-lg-1 flex-top">
-							<i className="fa fa-bullhorn fa-2x"></i>
-						</div>
-						<div className="col-10 col-lg-11">
-							Website under development. Enjoy the following overview and check
-							back with us soon:
-						</div>
-					</div>
+			<div className="col-12 no-pad landing-main-div">
+				<div className="col-lg-6 offset-lg-3 grey no-bottom-margin website-under-dev">
+					<h5 className="bold align-center">Website under development</h5>
+					<p className="italic no-bottom-margin align-center">
+						Enjoy the following overview and check back with us soon:
+					</p>
 				</div>
 
-				<div className="large-top-padding who-we-are">
+				<div className="container large-top-padding who-we-are">
 					<h5 className="green small-bottom-margin">Who we are</h5>
 					<h1 className="grey large-text">A general trading company</h1>
 					<p className="grey small-bottom-margin">
@@ -81,7 +76,7 @@ class Landing extends React.Component {
 					<div className="row">{this.renderWhoWeAreCards()}</div>
 				</div>
 
-				<div className="large-top-padding what-we-do">
+				<div className="container large-top-padding what-we-do">
 					<h5 className="bold green small-bottom-margin">What we do</h5>
 					<h1 className="grey large-text">Import and distribution</h1>
 					<p className="grey">
@@ -104,18 +99,19 @@ class Landing extends React.Component {
 					<p className="grey small-bottom-margin">
 						MakoBu understands the importance of reducing our carbon footprint
 						to the extent possible, therefore in order to contribute to this end
-						we import and distribute Clean Cook Stove (CCS) which utilizes
-						Ethanol, the virtually carbon-emission free household gas fuel, with
-						a goal of manufacturing these stoves locally starting in the near
-						future.
+						we import and distribute{" "}
+						<span className="green bold">Clean Cook Stove (CCS)</span> which
+						utilizes Ethanol, the virtually carbon-emission free household gas
+						fuel, with a goal of manufacturing these stoves locally starting in
+						the near future.
 					</p>
 				</div>
 
-				<div className="large-top-padding exportable-products">
+				<div className="container large-top-padding exportable-products">
 					<h5 className="bold green small-bottom-margin">
 						Our main exportable products
 					</h5>
-					<h1 className="grey large-text">We care about our resources</h1>
+					<h1 className="grey large-text">Sourcing with care</h1>
 					<p className="grey">
 						It is our mission to offer selective Premium Coffee with the best
 						characteristics in providing a beverage of ideal aroma and taste
@@ -124,14 +120,13 @@ class Landing extends React.Component {
 					<div className="row">{this.renderOurMainExportableProducts()}</div>
 				</div>
 
-				<div className="experience">
+				<div className="white-back experience">
 					<div className="align-center">
 						<Experience />
-						<p className="col-lg-8 offset-lg-2 grey italic experience-paragraph">
-							We have maintained a very important culture from day one - stay in
-							close relationship with small holder farmers in different regions
-							throughout the country and ascertain that we directly source and
-							export products of the highest quality.
+						<p className="col-lg-6 offset-lg-3 grey italic experience-paragraph">
+							Our close relationship with small holder farmers in a number
+							regions throughout the country, has put us in an ideal position to
+							directly source and export products of the highest quality.
 						</p>
 					</div>
 				</div>
