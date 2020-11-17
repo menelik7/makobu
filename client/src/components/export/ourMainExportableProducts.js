9f0756023690410467d473881f051fd16db5114d
Menelik Tefera <menelikworku@gmail.com>
2020-11-17 12:40:23 -0500

    Added favicons, edited content based on updated company profile which is missing the first 4 pages

--- client/src/components/export/ourMainExportableProducts.js
+++ client/src/components/export/ourMainExportableProducts.js
@@ -1,22 +1,22 @@
 export default [
 	{
 		picture: "unwashed",
-		title: "Unwashed coffee types",
+		title: "Unwashed coffee",
 		list: [
-			"Sidama - Grade 3 & 4",
-			"Nekemte - Grade 3 & 4",
-			"Jimma - Grade 3 & 4",
+			"Sidamo - Grade 3 & 4",
+			"Nekemte - Grade 3, 4 & 5",
+			"Jimma - Grade 4 & 5",
 		],
 	},
 	{
 		picture: "washed",
-		title: "Washed coffee types",
-		list: ["Yirgachefe - Grade 2", "Sidama - Grade 2", "Limu - Grade 2"],
+		title: "Washed coffee",
+		list: ["Yirgachefe - Grade 2", "Sidamo - Grade 2", "Limu - Grade 2"],
 	},
 	{
 		picture: "specialty",
-		title: "Specialty types",
-		list: ["Yirgachefe - Grade 1", "Giju - Grade 1"],
+		title: "Specialty coffee",
+		list: ["Yirgachefe - Grade 1", "Giju - Grade 1", "Sidamo - Grade 1"],
 	},
 	{
 		picture: "roasted-and-ground",
@@ -32,10 +32,13 @@ export default [
 		picture: "pulses",
 		title: "Pulses",
 		list: [
-			"Green Mung Bean",
-			"Kidney Bean",
-			"Soya Bean",
-			"White Pea Bean",
+			"Green Mung Beans",
+			"Pinto Beans",
+			"Kidney Beans",
+			"Red Speckled Kidney Beans",
+			"Light Speckled kidney Beans",
+			"Soya Beans",
+			"White Pea Beans",
 			"Desi Chickpea",
 			"Kabuli Chickpea",
 		],
@@ -43,6 +46,13 @@ export default [
 	{
 		picture: "spices",
 		title: "Spices",
-		list: ["Black Cumin", "Ginger", "Turmeric", "Cardamon"],
+		list: [
+			"Black Cumin",
+			"Cardamon",
+			"Coriander",
+			"Ginger",
+			"Turmeric",
+			"Fenugreek (Abish)",
+		],
 	},
 ];
